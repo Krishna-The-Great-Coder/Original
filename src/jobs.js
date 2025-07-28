4e98550705c4775334fbf6ac58c9481f817f5509
Krishna <kabirsaxena2007@gmail.com>
2025-07-28 15:45:32 +0000

    Added mustache files

---
+++ src/jobs.js
@@ -0,0 +1,32 @@
+const JOBS = [
+    {
+        id: 1,
+        title: 'Frontend Developer',
+        location: 'Bengaluru, India',
+        salary: '₹12,00,000',
+        posted: 'Mar 3, 2025'
+    },
+    {
+        id: 2,
+        title: 'Full Stack Developer',
+        location: 'New Delhi, India',
+        salary: '₹15,00,000',
+        posted: 'Feb 1, 2025'
+    },
+    {
+        id: 3,
+        title: 'Data Scientist',
+        location: 'San Francisco, USA',
+        salary: '$175,000',
+        posted: 'Dec 22, 2024'
+    },
+    {
+        id: 4,
+        title: 'Machine Learning Engineer',
+        location: 'Remote',
+        salary: '$80,000',
+        posted: 'Sep 19, 2024'
+    },
+]
+
+module.exports = JOBS;
